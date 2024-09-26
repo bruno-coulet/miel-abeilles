7d5d57803e1866ca5cd423d999ab6d117c3b813c
bruno-coulet-data <bruno.coulet.pro@gmail.com>
2024-09-26 15:04:37 +0200

    test de liste des meilleurs chemins

--- bee_generation.py
+++ bee_generation.py
@@ -21,13 +21,11 @@ def sort_distance_list() -> list:
     return sorted_distances_list
 
 def best_bees(CHOSEN_BEES):
-    # distance_list = sort_distance_list()
-    # return distance_list[0:BEST_BEES]
     best_bees = sort_distance_list()
     return best_bees[0:CHOSEN_BEES]
     
 
-def return_all_paths() -> list:
+def return_paths() -> list:
     path_list = []
     for bee in generate_bees():
         path_list.append(bee.gathering_path())
@@ -35,12 +33,13 @@ def return_all_paths() -> list:
 
 # def best_paths(CHOSEN_BEES):
 #     best_path_list = []
-#     for bee in best_bees():
-#         best_path_list.append(bee.gathering_path())
+#     for bee in best_bees(CHOSEN_BEES):
+#         best_path_list.append(path_list(best_bees))
+ 
 #     return best_path_list
 
 
 # print(sort_distance_list())
-print (f'\nLes {CHOSEN_BEES} meilleurs temps de parcours sont : {best_bees(CHOSEN_BEES)}')
-# print (return_all_paths())
-# print(best_path_list)
+print (f'\nLes {CHOSEN_BEES} meilleurs temps de parcours sont :\n {best_bees(CHOSEN_BEES)}')
+# print (return_paths())
+# print(best_paths(CHOSEN_BEES))
