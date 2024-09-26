200226854de3c3e5057f3bff24050a25da153db3
Ayoub Abderrahmane <ayoub-abderrahmane@laplateforme.io>
2024-09-26 13:30:25 +0200

    ajout fonction pour trier la liste en ordre croissant et une fonction qui renvoie un nombre souhaité des meilleurs abeilles selon leur distance parcourue

--- bee_generation.py
+++ bee_generation.py
@@ -8,7 +8,31 @@ def generate_100_bees() -> list:
         bees.append(Bee())
     return bees
 
-bees = generate_100_bees()
+# bees = generate_100_bees()
 
-for bee in bees:
-    print(bee.one_bee_gathers())
+def return_all_distances() -> list:
+    distance_list = []
+    for bee in generate_100_bees():
+        distance_list.append(bee.gathering_distance())
+    return distance_list
+
+def sort_distance_list() -> list:
+    distance_list = return_all_distances()
+    distance_list.sort()
+    return distance_list
+
+def choose_best_bees(rate):
+    distance_list = sort_distance_list()
+    return distance_list[0:rate]
+    
+
+def return_all_paths() -> list:
+    path_list = []
+    for bee in generate_100_bees():
+        path_list.append(bee.gathering_path())
+    return path_list
+
+
+
+# print(sort_distance_list())
+print (choose_best_bees(20))
