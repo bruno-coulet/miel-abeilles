2d4ee39b835bf9cb9b3a76130e35e20ecb75f9e3
bruno-coulet-data <bruno.coulet.pro@gmail.com>
2024-10-01 15:31:12 +0200

    stash

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from beehive import Beehive
-from constants import POPULATION_SIZE, SELECTION, INTERLIGNE
+from constants import POPULATION_SIZE, SELECTION, INTERLIGNE, GENERATION_COUNT
 
 def main():
-    print(INTERLIGNE)
-    print("-" * 36, "main.py", "-" * 36)
-    print(INTERLIGNE)
 
     beehive = Beehive(POPULATION_SIZE)
-
-    print("\nPopulation : ", POPULATION_SIZE, " abeilles")
-    print("Selection  : ", SELECTION, " abeilles\n")
-
-    print(INTERLIGNE)
+    print("\nPopulation : ", POPULATION_SIZE, " abeilles\nSelection  : ", SELECTION, " abeilles\n")
 
     beehive.generate_bees()
 
@@ -21,10 +14,6 @@ def main():
     for bee in selected_bees:
         print(f"L'abeille {bee.bee_id} a parcourue une distance de {bee.total_distance()}\n")
 
-
-    print(INTERLIGNE)
-
-
     print(f"\nChemins des {SELECTION} meilleures abeilles :\n")
 
     selected_paths = beehive.selected_paths()
@@ -32,13 +21,14 @@ def main():
         print(f"Abeille {bee.bee_id},\n chemin : {bee.gathering_path()}\n")
 
 
-    print(INTERLIGNE)
+        print(INTERLIGNE)
 
 
-    modified_bees = beehive.modify_first_to_last(selected_paths)
+    for i in range(GENERATION_COUNT):
+        modified_bees = beehive.modify_first_to_last(selected_paths)
 
-    print("\nOriginal paths:\n", [bee.gathering_path() for bee in modified_bees])
-    print("\nModified paths:\n", [bee.modified_path for bee in modified_bees])
+        print("\nOriginal paths:\n", [bee.gathering_path() for bee in modified_bees])
+        print("\nModified paths:\n", [bee.modified_path for bee in modified_bees])
 
 if __name__ == "__main__":
     main()
