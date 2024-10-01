a27b3666d891a5466a1cbfd68b5ae7061f173ee1
bruno-coulet-data <bruno.coulet.pro@gmail.com>
2024-10-01 10:22:56 +0200

    classe beehive

--- bee.py
+++ bee.py
@@ -1,4 +1,4 @@
-import random, math, csv
+import random, math
 from constants import FLOWERS, POPULATION_SIZE, SELECTION, REJECTION
 
 class Bee():
@@ -32,7 +32,7 @@ class Bee():
         # print(f"\n'total_distance'-> Distance totale parcourue : {total_distance}")
         return total_distance
 
-    def gathering_path(self) -> list[(int, int)]:
+    def gathering_path(self) -> list[tuple[int, int]]:
         """ Returns the path of flower collection """
         self.path = self.flowers_list
         return self.path
@@ -46,61 +46,67 @@ memorized_paths = []
 generated_bees = []
 
 #---------------------First Generation---------------------------------
+class Beehive:
+    def __init__(self, population_size, selected=[], modified=[]):
+        self.population_size = population_size
+        self.selected = selected
+        self.modified = modified
+        self.generated_bees = []
+        self.memorized_paths = []
+
+    def generate_bees(self):
+        '''Generates as many bees as defined in population_size'''
+        self.generated_bees = [Bee(i) for i in range(self.population_size)]
+        return self.generated_bees
 
 
-def generate_bees(POPULATION_SIZE) -> list[Bee]:
-    ''' generates as many bees as defined in POPULATION '''
-    generated_bees.extend([Bee(i) for i in range(POPULATION_SIZE)])
-    # POPULATION_SIZE -= REJECTION
-    return generated_bees
-# print(generate_bees(POPULATION_SIZE))
+    def sorted_distances(self) -> list[tuple[float, Bee]]:
+        """Tuples (distance, bee), sorted by the distance."""
+        if not self.generated_bees:
+            self.generate_bees()  # Ensure bees are generated
+        distances_with_bees = [(bee.total_distance(), bee) for bee in self.generated_bees]
+        return sorted(distances_with_bees, key=lambda x: x[0])
+    # print(sorted_distances())
 
 
-def sorted_distances() -> list[tuple[float, Bee]]:
-    """tuples (distance, bee), sorted by the distance."""
-    distances_with_bees = [(bee.total_distance(), bee) for bee in generate_bees(POPULATION_SIZE)]
-    return sorted(distances_with_bees, key=lambda x: x[0])
-# print(sorted_distances())
-
-
-
-sorted_bees = sorted_distances()
-
-
-
-
-def selected_bees(SELECTION) -> list[Bee]:
-    sorted_bee_distances = sorted_distances() 
-    return [bee for _, bee in sorted_bee_distances[:SELECTION]]
-
-def rejected_bees(REJECTION) -> list[Bee]:
-    rejected_bee_distances = sorted_distances()
-    return [bee for _, bee in rejected_bee_distances[-REJECTION:]]
-
-def selected_paths(SELECTION) -> list[Bee]:
-    ''' lists the paths of the best bees '''
-    selected_paths = []
-    chosen_bees = selected_bees(SELECTION)
-    for bee in chosen_bees:
-        selected_paths.append(bee)
-        memorized_paths.append(bee.gathering_path())
-    return selected_paths
-# print("\nselected_paths (liste d'objets bee):\n\n", selected_paths(SELECTION))
+    self.sorted_bees = sorted_distances()
 
 
 
-#-----------------------Modifications--------------------------------------------
+
+    def selected_bees(SELECTION) -> list[Bee]:
+        sorted_bee_distances = self.sorted_distances() 
+        return [bee for _, bee in sorted_bee_distances[:SELECTION]]
+
+    def rejected_bees(REJECTION) -> list[Bee]:
+        rejected_bee_distances = sorted_distances()
+        return [bee for _, bee in rejected_bee_distances[-REJECTION:]]
+
+    def selected_paths(SELECTION) -> list[Bee]:
+        ''' lists the paths of the best bees '''
+        selected_paths = []
+        chosen_bees = selected_bees(SELECTION)
+        for bee in chosen_bees:
+            selected_paths.append(bee)
+            memorized_paths.append(bee.gathering_path())
+        return selected_paths
+    # print("\nselected_paths (liste d'objets bee):\n\n", selected_paths(SELECTION))
 
 
-def modify_first_to_last(selected_bees: list[Bee]) -> list[Bee]:
-    for bee in selected_bees:
-        # creates a shallow copy of the path
-        path = bee.gathering_path()[:]
-        path[0], path[-1] = path[-1], path[0]
-        # assign the modified path back to bee.path
-        bee.modified_path = path
-        modified_bees = selected_bees
-    return modified_bees  # Return the modified bees
+
+    #-----------------------Modifications--------------------------------------------
+
+
+    def modify_first_to_last(self, selected_bees: list[Bee]) -> list[Bee]:
+        modified_bees = []
+        for bee in selected_bees:
+            # creates a shallow copy of the path
+            path = bee.gathering_path()[:]
+            path[0], path[-1] = path[-1], path[0]
+            # Assign modified path to the bee
+            bee.modified_path = path
+            modified_bees.append(bee)
+        return modified_bees
 
 
 # ------------------------Second generation----------------------------------------
--- main.py
+++ main.py
@@ -1,41 +1,31 @@
 from bee import Bee, selected_bees, selected_paths, rejected_bees, memorized_paths, modify_first_to_last
-from constants import  FLOWERS, POPULATION_SIZE, SELECTION, REJECTION, INTERLIGNE
+from constants import FLOWERS, POPULATION_SIZE, SELECTION, REJECTION, INTERLIGNE
 
-print(INTERLIGNE, INTERLIGNE)
-print(INTERLIGNE,"Début", INTERLIGNE)
-print(INTERLIGNE, INTERLIGNE)
+def main():
+    print(INTERLIGNE)
+    print("-" * 36, "main.py", "-" * 36)
+    print(INTERLIGNE)
 
+    print("\nPopulation : ", POPULATION_SIZE, " abeilles")
+    print("Selection  : ", SELECTION, " abeilles\n")
 
-print("\nPopulation : ",POPULATION_SIZE," abeilles")
-print("Selection  : ",SELECTION," abeilles\n")
+    print(INTERLIGNE)
 
-print(INTERLIGNE)
+    print(f"\nLes {SELECTION} meilleures abeilles et leurs distances :")
+    for bee in selected_bees(SELECTION):
+        print(f"L'abeille {bee.bee_id} a parcourue une distance de {bee.total_distance()}\n")
 
-print(f"\nLes {SELECTION} meilleures abeilles et leurs distances :\n")
-for bee in selected_bees(SELECTION): 
-    print(f"l'abeille {bee.bee_id} a parcourue une distance de {bee.total_distance()}")
+    print(INTERLIGNE)
 
-
-print(INTERLIGNE)
-
-print(f"\nChemins des {SELECTION} meilleures abeilles :\n")
-for bee in selected_paths(SELECTION):
+    print(f"\nChemins des {SELECTION} meilleures abeilles :\n")
+    for bee in selected_paths(SELECTION):
         print(f"Abeille {bee.bee_id},\n chemin : {bee.gathering_path()}\n")
-        
-print(INTERLIGNE)
 
+    print(INTERLIGNE)
 
-# print("Elimination : ",REJECTION," abeilles\n")
-# for _, bee in rejected_bees(REJECTION): 
-#     print(f"L'abeille {bee.bee_id} a parcourue une distance de {bee.total_distance()}, nous allons la remplacer")
+    modified_bees = modify_first_to_last(selected_paths(SELECTION))
+    print("\nOriginal paths:\n", [bee.gathering_path() for bee in modified_bees])
+    print("\nModified paths:\n", [bee.modified_path for bee in modified_bees])
 
-
-modified_bees = modify_first_to_last(selected_paths(SELECTION))
-print("\nOriginal paths:\n", [bee.gathering_path() for bee in modified_bees])
-print("\nModified paths:\n", [bee.modified_path for bee in modified_bees])
-
-print(INTERLIGNE)
-print(INTERLIGNE)
-print(INTERLIGNE)
-
-
+if __name__ == "__main__":
+    main()
