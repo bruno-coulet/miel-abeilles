9e3808e4709d761011473126e83dd6462f605ebb
bruno-coulet-data <bruno.coulet.pro@gmail.com>
2024-10-02 14:59:54 +0200

    création des chemins de child_1 et de child_2

--- beehive.py
+++ beehive.py
@@ -2,128 +2,111 @@ import random, math
 from constants import FLOWERS, POPULATION_SIZE, SELECTION, REJECTION
 
 class Bee():
+    def __init__(self, bee_distance = 0, bee_path = []):
+        self.path = FLOWERS.copy()
+        random.shuffle(self.path)
+        self.compute_distance()
 
-    def __init__(self, bee_id, bee_distance = 0, bee_path = [], modified_path = []):
-        self.bee_id = bee_id
-        self.flowers_list = FLOWERS.copy()
-        random.shuffle(self.flowers_list)
-        self.distance = bee_distance
-        self.path = bee_path
-        self.modified_path = modified_path
-    
     def distance_a_to_b(self ,a ,b) -> float:
         """Calculates the Euclidean distance between two points."""
         stage_distance = round(math.sqrt((b[0] - a[0])**2 + (b[1] - a[1])**2),2)
         return stage_distance
 
-    def total_distance(self) -> float:
+    def compute_distance(self) -> float:
         """Calculates the total distance from the hive, through every shuffled flower,back to the hive."""
         hive = (500, 500)
         current_position = hive
         total_distance = 0
 
-        for flower in self.flowers_list:
+        for flower in self.path:
             stage_distance = self.distance_a_to_b(current_position, flower)
             total_distance += stage_distance
             current_position = flower
         
         return_to_hive_distance = self.distance_a_to_b(current_position, hive)
         total_distance += return_to_hive_distance
-        total_distance = round(total_distance,2)
+        # total_distance = round(total_distance,2)
             
         # print(f"\n'total_distance'-> Distance totale parcourue : {total_distance}")
-        return total_distance
-
-    def gathering_path(self) -> list[tuple[int, int]]:
-        """ Returns the path of flower collection """
-        self.path = self.flowers_list
-        return self.path
+        self.distance = round(total_distance,2)
     
     def __repr__(self) -> str:
-        return f"Bee id : {self.bee_id}, \nBee path : {self.gathering_path()}\n"
+        return f"\npath : {self.path}, distance : {self.distance}"
 
 
 
 #---------------------First Generation---------------------------------
 class Beehive:
-
     def __init__(self, population_size):
         self.population_size = population_size
         self.generate_bees()
-        self.memorized_paths = []
-        # self.cross_bees()
+        self.select_bees()
 
-    def generate_bees(self):
+    def generate_bees(self) -> None:
         '''Generates as many bees as defined in population_size'''
-        self.generated_bees = [Bee(i) for i in range(self.population_size)]
-        return self.generated_bees
-
-    def sorted_bees(self) -> list[tuple[float, Bee]]:
-        """Tuples (distance, bee object), sorted by the distance."""
-        if not self.generated_bees:
-            self.generate_bees()
-        distances_with_bees = [(bee.total_distance(), bee) for bee in self.generated_bees]
-        return sorted(distances_with_bees, key=lambda x: x[0])
-    # print(sorted_bees())
-
-    def selected_bees(self) -> list[Bee]:
-        sorted_bee_distances = self.sorted_bees()
-        return [bee for _, bee in sorted_bee_distances[:SELECTION]]
-
-    def rejected_bees(self) -> list[Bee]:
-        rejected_bee_distances = self.sorted_bees()
-        return [bee for _, bee in rejected_bee_distances[-REJECTION:]]
+        self.bees = [Bee(i) for i in range(self.population_size)]
     
-    def selected_paths(self) -> list[Bee]:
-        ''' lists the paths of the best bees '''
-        selected_paths = []
-        chosen_bees = self.selected_bees()
-        for bee in chosen_bees:
-            selected_paths.append(bee)
-            self.memorized_paths.append(bee.gathering_path())
-        return selected_paths
-    # print("\nselected_paths (liste d'objets bee):\n\n", selected_paths(SELECTION))
+    def select_bees(self) -> None:
+        self.bees.sort(key = lambda bee:bee.distance)
+
+    def get_bees(self) -> list[Bee]:
+        return self.bees
 
 
-
+    
     #-----------------------Cross over--------------------------------------------
+    # dans le main, il y a    print(beehive.bees)
 
     def cross_bees(self):
-        # for bee in range(SELECTION):
-        # for bee in self.selected_bee:
 
-        rejected_bees_id = []
-        print("Les id des rejected_bees sont : \n")
-        for bee in self.rejected_bees():
-            rejected_bees_id.append(bee.bee_id)
-        print(rejected_bees_id)
+        for x in range(0,SELECTION+1,2):
 
-        child_1 = Bee(rejected_bees_id[0])
+            child = Bee()
 
-        # FOR 50 FLOWERS
-        # parent_1_half_1 = self.memorized_paths[0][0:25]
-        # parent_2_half_2 = self.memorized_paths[1][25:-1]
-        # FOR 100 FLOWERS
-        parent_1_half_1 = self.memorized_paths[0][0:3]
-        parent_2_half_2 = self.memorized_paths[1][3:]
+            parent_1 = self.bees[x]
+            parent_2 = self.bees[x+1]
+            print("parent_1",parent_1)
+            print("parent_2",parent_2)
+            print()
+            # FOR 6 FLOWERS
+            parent_1_half_1 = parent_1.path[0:3]
+            parent_2_half_2 = parent_2.path[3:]
+            child.path = [parent_1_half_1 + parent_2_half_2]
+            print("child.path : ",child.path)
+            print()
 
-        child_1.bee_path = [parent_1_half_1 + parent_2_half_2]
 
-        print("child_1 id : ",child_1.bee_id,"\n",\
-        "abeille ",child_1.bee_id," : ","path = ",child_1.bee_path,"\n",\
-        "abeille ",child_1.bee_id," : ","distance = ",child_1.total_distance(),"\n")
-        
+ 
 
-        child_2 = Bee(rejected_bees_id[1])
+        # for bee in self.bees:
 
-        parent_1_half_2 = self.memorized_paths[0][25:-1]
-        parent_2_half_1 = self.memorized_paths[1][0:25]
 
-        child_2.bee_path = [parent_1_half_2 + parent_2_half_1]
-  
-        print("child_2 id : ",child_2.bee_id,"\n",\
-        "abeille ",child_2.bee_id," : ","path = ",child_2.bee_path,"\n",\
-        "abeille ",child_2.bee_id," : ","distance = ",child_2.total_distance(),"\n")
+        #     child = bee
+
+
+        #     # FOR 50 FLOWERS
+        #     # parent_1_half_1 = self.memorized_paths[0][0:25]
+        #     # parent_2_half_2 = self.memorized_paths[1][25:-1]
+        #     # FOR 6 FLOWERS
+        #     parent_1_half_1 = self.bees.path[0][0:3]
+        #     parent_2_half_2 = self.bees.path[1][3:]
+
+        #     child.path = [parent_1_half_1 + parent_2_half_2]
+
+        #     print("child_1\npath = ",child. path,"\ndistance = ",child.distance,"\n")
+            
+
+        #     child_2 = Bee()
+        #     # FOR 50 FLOWERS
+        #     # parent_1_half_2 = self.memorized_paths[0][25:-1]
+        #     # parent_2_half_1 = self.memorized_paths[1][0:25]
+        #     # FOR 6 FLOWERS
+        #     parent_1_half_2 = self.bees.paths[0][3:]
+        #     parent_2_half_1 = self.bees.paths[1][0:3]
+
+        #     child_2.bee_path = [parent_1_half_2 + parent_2_half_1]
+    
+        #     print("child_2\npath = ",child_2.bee_path,"\ndistance = ",child_2.distance,"\n")
 
 
 
@@ -149,7 +132,3 @@ class Beehive:
 # la 51eme prend le chemin de la première et swape la 1ère et la dernière fleur
 # la 52eme prend le chemin de la 2ère et swape la 1ère et la dernière fleur
 #  etc...
-
-
-
-
--- main.py
+++ main.py
@@ -4,28 +4,18 @@ from constants import POPULATION_SIZE, SELECTION, INTERLIGNE, GENERATION_COUNT
 def main():
 
     beehive = Beehive(POPULATION_SIZE)
+
     print("\nPopulation : ", POPULATION_SIZE, " abeilles\nSelection  : ", SELECTION, " abeilles\n")
 
-    beehive.generate_bees()
-    # selected_bees = beehive.selected_bees()
+    # beehive.generate_bees() EST DEJA DANS LE CONSTRUCTEUR  
 
-    print(INTERLIGNE)
-
-    print(f"\nChemins des {SELECTION} meilleures abeilles avant cross_over:\n")
-    selected_paths = beehive.selected_paths()
-    for bee in selected_paths:
-        print(f" chemin de l'abeille {bee.bee_id} : {bee.gathering_path()}\ndistance de l'abeille {bee.bee_id} : {bee.total_distance()}\n\n")
-
-
-    print(INTERLIGNE)
-
+    print(f"\nChemins AVANT cross_over:\n") 
+    print("beehive.bees : ",beehive.bees)
 
     # for i in range(GENERATION_COUNT):
-    print(f"\nChemins apres cross_over:\n")    
+    print(f"\nChemins APRES cross_over:\n")    
     beehive.cross_bees()
 
-        # print("\nOriginal paths:\n", [bee.gathering_path() for bee in modified_bees])
-        # print("\nModified paths:\n", [bee.modified_path for bee in modified_bees])
 
 if __name__ == "__main__":
     main()
